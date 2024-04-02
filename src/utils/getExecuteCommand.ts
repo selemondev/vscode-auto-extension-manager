52246cfdf1456c52f2319fc4cc39a64e29e58f2c
Selemon Dev <selemon@nathandigital.com>
2024-04-02 15:11:28 +0300

    feat: add dependency detection feature

--- src/utils/getExecuteCommand.ts
+++ src/utils/getExecuteCommand.ts
@@ -1,10 +1,21 @@
 import * as vscode from "vscode";
 
-export const executeCommand = (cmd: string, newTerminal = true) => {
+export const executeInstallCommand = (cmd: string, newTerminal = true) => {
     let terminal = vscode.window.activeTerminal;
 
     if (!terminal || newTerminal) {
         terminal = vscode.window.createTerminal('Extension Installer');
+    };
+
+    terminal.show();
+    terminal.sendText(cmd);
+}
+
+export const executeUnInstallCommand = (cmd: string, newTerminal = true) => {
+    let terminal = vscode.window.activeTerminal;
+
+    if (!terminal || newTerminal) {
+        terminal = vscode.window.createTerminal('Extension Uninstaller');
     };
 
     terminal.show();
