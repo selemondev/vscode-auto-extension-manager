e6b444e4edfbbd9b63354a6a0e1893ac6b446d81
Selemondev <selemondev@selemondev.local>
2024-04-02 23:34:56 +0300

    feat: add VSCode Extensions

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from "vscode";
 import { executeInstallCommand, executeUnInstallCommand } from "./utils/getExecuteCommand";
-import { getExtensionIds } from "./utils/getExtensionIds";
 import { getProjectDependencies } from "./utils/getProjectDeps";
+import { getProjectExtensionIds } from "./utils/getProjectExtensionIds";
 import { getProjectDirectory } from "./utils/getProjectDir";
 import { execShell } from "./utils/getExecShellCmd";
 let installedExtensions: string[] = [];
+let extensionsToInstall: string[] = [];
+let extensionsToUninstall: string[] = [];
 const getAllExtensions = async () => {
     try {
         const output = await execShell('code --list-extensions');
@@ -12,98 +14,49 @@ const getAllExtensions = async () => {
 		installedExtensions.length = 0;
         installedExtensions.push(...extensionIds);
     } catch (error) {
-        console.error('Error:', error);
         vscode.window.showErrorMessage('Failed to get installed extensions.');
     }
 };
 
-export async function activate(context: vscode.ExtensionContext) {
-	await getAllExtensions();
-	let extensionIds: string[] = [];
-	const dependencies = await getProjectDependencies();
-	for (const dep of dependencies) {
-		if (dep.name) {
-			const extensions = getExtensionIds[dep.name];
-			if (extensions) {
-				extensionIds.push(...extensions.extensionIds);
-			}
-		};
-	};
-	const extensionsToInstall = extensionIds.filter((ext) => !installedExtensions.includes(ext));
-	const extensionsToUninstall = installedExtensions.filter((ext) => !extensionIds.includes(ext));
-	// vscode.window.showInformationMessage('To install: ', `${extensionsToInstall.join(', ')}` )
-	// vscode.window.showInformationMessage('To uninstall: ', `${extensionsToUninstall.join(', ')}` )
-	const uninstallExtensions = () => {
-		const extensionToUnInstall = extensionsToUninstall.map((ext) => `--uninstall-extension ${ext}`).join(' ');
-		executeUnInstallCommand(`code ${extensionToUnInstall}`);
-	};
+const uninstallExtensions = () => {
+	const extensionsToRemove = extensionsToUninstall.map((ext) => `--uninstall-extension ${ext}`).join(' ');
+	executeUnInstallCommand(`code ${extensionsToRemove}`);
+};
 
-	const installExtensions = () => {
-		const extensions = extensionsToInstall.map((ext) => `--install-extension ${ext}`).join(' ');
-		if(extensionsToInstall.length) {
-			executeInstallCommand(`code ${extensions}`);
-		}
-	};
+const installExtensions = () => {
+	const extensions = extensionsToInstall.map((ext: string) => `--install-extension ${ext}`).join(' ');
+	if(extensionsToInstall.length) {
+		executeInstallCommand(`code ${extensions}`);
+	}
+};
+
+const manageExtensions = async () => {
+	await getAllExtensions();
+	const dependencies = await getProjectDependencies();
+	const extensionIds = getProjectExtensionIds(dependencies);
+	extensionsToInstall = extensionIds.filter((ext: string) => !installedExtensions.includes(ext));
+	extensionsToUninstall = installedExtensions.filter((ext) => !extensionIds.includes(ext));
 	if(extensionsToUninstall.length){
-		vscode.window.showInformationMessage('Removing extensions....');
 		uninstallExtensions();
 	};
 
 	if(extensionsToInstall.length) {
-		vscode.window.showInformationMessage('Installing extensions....');
 		installExtensions();
 	}
+};
+
+export async function activate(context: vscode.ExtensionContext) {
+	await manageExtensions();
 	const fileWatcher = vscode.workspace.createFileSystemWatcher(`${getProjectDirectory()}/package.json`);
 	fileWatcher?.onDidDelete(() => {
 		vscode.window.showErrorMessage('Removing extensions.....');
 		uninstallExtensions();
 	});
 	fileWatcher?.onDidChange(async () => {
-		await getAllExtensions();
-		let extensionIds: string[] = [];
-		const dependencies = await getProjectDependencies();
-		for (const dep of dependencies) {
-			if (dep.name) {
-				const extensions = getExtensionIds[dep.name];
-				if (extensions) {
-					extensionIds.push(...extensions.extensionIds);
-				}
-			};
-		};
-		const extensionsToInstall = extensionIds.filter((ext) => !installedExtensions.includes(ext));
-		const extensionsToUninstall = installedExtensions.filter((ext) => !extensionIds.includes(ext));
-		vscode.window.showInformationMessage('To install: ', `${extensionsToInstall.join(', ')}` )
-		vscode.window.showInformationMessage('To uninstall: ', `${extensionsToUninstall.join(', ')}` )
-		vscode.window.showInformationMessage('Extension IDS: ', `${extensionIds.join(', ')}` )
-		const uninstallExtensions = () => {
-			const extensionsToRemove = extensionsToUninstall.map((ext) => `--uninstall-extension ${ext}`).join(' ');
-			executeUnInstallCommand(`code ${extensionsToRemove}`);
-		};
-	
-		const installExtensions = () => {
-			const extensions = extensionsToInstall.map((ext) => `--install-extension ${ext}`).join(' ');
-			if(extensionsToInstall.length) {
-				executeInstallCommand(`code ${extensions}`);
-			}
-		};
-		if(extensionsToUninstall.length){
-			vscode.window.showInformationMessage('Removing extensions....');
-			uninstallExtensions();
-		};
-	
-		if(extensionsToInstall.length) {
-			vscode.window.showInformationMessage('Installing extensions....');
-			installExtensions();
-		}
+		await manageExtensions();
 	});
-
-	vscode.workspace.onDidChangeWorkspaceFolders(installExtensions);
 	context.subscriptions.push(fileWatcher);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {
-
-}
-
-
+export function deactivate() {}
